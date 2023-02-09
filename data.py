6a937385e1fc644a72148dfa29526dc7fbc80e61
Wishmitha Mendis <wishmitha.14@cse.mrt.ac.lk>
2023-02-09 23:48:28 +0000

    implement data augmentation

--- data.py
+++ data.py
@@ -1,17 +1,83 @@
-import pandas as pd
+import numpy as np
+import json
 
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import re
 
-def get_projects():
-    # Extract projects
-    PROJECTS_URL = "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/projects.csv"
-    return pd.read_csv(PROJECTS_URL)
+nltk.download("stopwords")
+STOPWORDS = stopwords.words("english")
+stemmer = PorterStemmer()
 
+def clean_text(text, lower=True, stem=False, stopwords=STOPWORDS):
+    """Clean raw text."""
+    # Lower
+    if lower:
+        text = text.lower()
 
-def get_tags():
-    # Extract tags
-    TAGS_URL = "https://raw.githubusercontent.com/GokuMohandas/Made-With-ML/main/datasets/tags.csv"
-    return pd.read_csv(TAGS_URL)
+    # Remove stopwords
+    if len(stopwords):
+        pattern = re.compile(r'\b(' + r"|".join(stopwords) + r")\b\s*")
+        text = pattern.sub('', text)
 
+    # Spacing and filters
+    text = re.sub(
+        r"([!\"'#$%&()*\+,-./:;<=>?@\\\[\]^_`{|}~])", r" \1 ", text
+    )  # add spacing between objects to be filtered
+    text = re.sub("[^A-Za-z0-9]+", " ", text)  # remove non alphanumeric chars
+    text = re.sub(" +", " ", text)  # remove multiple spaces
+    text = text.strip()  # strip white space at the ends
 
-def get_df():
-    return pd.merge(get_projects(), get_tags(), on="id")
+    # Remove links
+    text = re.sub(r"http\S+", "", text)
+
+    # Stemming
+    if stem:
+        text = " ".join([stemmer.stem(word, to_lowercase=lower) for word in text.split(" ")])
+
+    return text
+
+class LabelEncoder(object):
+    """Encode labels into unique indices"""
+    def __init__(self, class_to_index={}):
+        self.class_to_index = class_to_index or {}  # mutable defaults ;)
+        self.index_to_class = {v: k for k, v in self.class_to_index.items()}
+        self.classes = list(self.class_to_index.keys())
+
+    def __len__(self):
+        return len(self.class_to_index)
+
+    def __str__(self):
+        return f"<LabelEncoder(num_classes={len(self)})>"
+
+    def fit(self, y):
+        classes = np.unique(y)
+        for i, class_ in enumerate(classes):
+            self.class_to_index[class_] = i
+        self.index_to_class = {v: k for k, v in self.class_to_index.items()}
+        self.classes = list(self.class_to_index.keys())
+        return self
+
+    def encode(self, y):
+        encoded = np.zeros((len(y)), dtype=int)
+        for i, item in enumerate(y):
+            encoded[i] = self.class_to_index[item]
+        return encoded
+
+    def decode(self, y):
+        classes = []
+        for i, item in enumerate(y):
+            classes.append(self.index_to_class[item])
+        return classes
+
+    def save(self, fp):
+        with open(fp, "w") as fp:
+            contents = {"class_to_index": self.class_to_index}
+            json.dump(contents, fp, indent=4, sort_keys=False)
+
+    @classmethod
+    def load(cls, fp):
+        with open(fp, "r") as fp:
+            kwargs = json.load(fp=fp)
+        return cls(**kwargs)
